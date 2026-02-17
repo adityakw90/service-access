8e9834b8227428f887d179da5f058c5dcbdefd2a
Aditya <aditya.kw90@gmail.com>
2026-02-17 15:42:49 +0700

    resolver port

---
+++ internal/core/port/resolver/permission.go
@@ -0,0 +1,8 @@
+package resolver
+
+import "context"
+
+type PermissionResolver interface {
+	IDsByUIDs(ctx context.Context, uids []string) (map[string]int64, error)
+	UIDsByIDs(ctx context.Context, ids []int64) (map[int64]string, error)
+}
