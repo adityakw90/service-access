c53d0449bc4fc4fb271adb0fa09b7c00cb4b528b
Aditya <aditya.kw90@gmail.com>
2026-02-18 16:48:11 +0700

    port repo

---
+++ internal/core/port/repository/unitofwork.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"context"
+)
+
+type Repositories interface {
+	Permission() PermissionRepository
+	Group() GroupRepository
+	Role() RoleRepository
+	Subject() SubjectRepository
+}
+
+type UnitOfWork interface {
+	Do(ctx context.Context, fn func(r Repositories) error) error
+}
